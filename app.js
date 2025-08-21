a9a51ec7899a7f4fde8094b2cd3a20b3df124928
Abourhim <abdobourhim141@gmail.com>
2025-08-20 21:46:21 -0400

    managed to show map

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 //Variables
+const map = L.map("map");
 
-//map generate
-let map = L.map("map").setView([51.505, -0.09], 13);
+map.setView([33.9737, -118.363], 7);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
+
+L.marker([34.0515, -118.2225]).addTo(map);
+// .bindPopup("I could be here LOL.")
+// .openPopup();
